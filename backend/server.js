cfc05176bd5f5818ff73c4ce6d85a25623639885
Komal Ladhva <komalladhva2926@gmail.com>
2022-02-27 18:56:24 +0530

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const databaseConnect = require('./Config/databaseConnect');
 const UserRouter = require('./Router/UserRouter');
-const noteRouter=require('./Router/noteRouter');
+const noteRouter = require('./Router/noteRouter');
 const { notFound, ErrorHandler } = require('./errorMiddleware/ErrorMiddleware');
+const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -12,17 +13,27 @@ databaseConnect();
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
-})
-
-// app.get("/api/notes", (req, res) => {
-//     res.json(notes);
-// })
-
 app.use('/api/user', UserRouter);
 
 app.use('/api/notes', noteRouter);
+
+// -------- deployment --------------
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+else {
+    app.get("/", (req, res) => {
+        res.send("API is running...");
+    })
+}
+
+// -------- deployment --------------
 
 const PORT = process.env.PORT || 5000;
 
